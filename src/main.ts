fc9e688aed5fafb36d58b5b42ec9182a6277deb1
ishida <webmaster@joe1cafe.com>
2022-12-30 00:23:43 +0900

    adjust wireframe

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,57 @@
-import { app, BrowserWindow } from "electron";
+import { app, BrowserWindow, globalShortcut } from "electron";
 import * as path from "path";
+import { autoUpdater } from "electron-updater";
+import log from "electron-log";
+
+class AppUpdater {
+  constructor() {
+    log.transports.file.level = 'info';
+    autoUpdater.logger = log;
+    autoUpdater.checkForUpdatesAndNotify();
+  }
+}
+
+let mainWindow: Electron.BrowserWindow;
 
 function createWindow() {
   // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    height: 600,
+  mainWindow = new BrowserWindow({
+    height: 768,
+    width: 1024,
+    resizable: false,
+    transparent: false,
+    kiosk: false,
+    center: true,
+    fullscreen: false,
+    // frame: false,
+    movable: false,
+    show: false,
+
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
+      nodeIntegration: true,
+      contextIsolation: false,
     },
-    width: 800,
   });
 
+  mainWindow.setMenu(null);
+  mainWindow.removeMenu();
+
+  const startUrl = process.env.WEB_URL || path.join(__dirname, "../index.html");
+
   // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, "../index.html"));
+  mainWindow.loadFile(startUrl);
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
+
+  mainWindow.on("close", () => (mainWindow = null));
+
+  mainWindow.on("ready-to-show", mainWindow.show);
+
+  // Remove this if your app does not use auto updates
+  // eslint-disable-next-line
+  new AppUpdater();
 }
 
 // This method will be called when Electron has finished
@@ -27,9 +63,28 @@ app.whenReady().then(() => {
   app.on("activate", function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    if (BrowserWindow.getAllWindows().length === 0 || mainWindow === null)
+      createWindow();
   });
-});
+
+  app.on('browser-window-focus', function () {
+    globalShortcut.register("CommandOrControl+R", () => {
+      console.log("CommandOrControl+R is pressed: Shortcut Disabled");
+    });
+    globalShortcut.register("F5", () => {
+      console.log("F5 is pressed: Shortcut Disabled");
+    });
+    globalShortcut.register("CommandOrControl+F5", () => {
+      console.log("CommandOrControl+F5 is pressed: Shortcut Disabled");
+    });
+  });
+  app.on('browser-window-blur', function () {
+    globalShortcut.unregister('CommandOrControl+R');
+    globalShortcut.unregister('F5');
+    globalShortcut.unregister('CommandOrControl+F5');
+  });
+
+}).catch(console.log);
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
@@ -42,3 +97,8 @@ app.on("window-all-closed", () => {
 
 // In this file you can include the rest of your app"s specific main process
 // code. You can also put them in separate files and require them here.
+
+const isSingleInstance = app.requestSingleInstanceLock()
+if (!isSingleInstance) {
+  app.quit()
+}
